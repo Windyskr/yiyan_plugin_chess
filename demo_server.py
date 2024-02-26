67340b6b8ebd1d37b676b67d8873e6f9cfb87af8
Yan <oyyfcy@outlook.com>
2024-02-26 20:57:07 +0800

    Changes

--- demo_server.py
+++ demo_server.py
@@ -5,11 +5,15 @@ from flask import Flask, request, send_file, make_response
 from flask_cors import CORS
 import json
 import random
+import sqlite3
+
+DATABASE_URL = "chesstest.db"
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://yiyan.baidu.com"}})
 
 wordbook = []
+
 
 def make_json_response(data, status_code=200):
     response = make_response(json.dumps(data), status_code)
@@ -17,10 +21,57 @@ def make_json_response(data, status_code=200):
     return response
 
 
+# 开始一盘游戏
+@app.route("/start_game", methods=['POST'])
+async def start_game():
+    """
+        开始一盘游戏
+    """
+    # 输出所有headers参数
+    # print(request.headers)
+    # 输出headers中的X-Bd-Plugin-Sessionidhash参数
+    print(request.headers.get('X-Bd-Plugin-Sessionidhash'))
+    return make_json_response({"message": "五子棋游戏开始成功"})
+    # return make_json_response({"message": "游戏开始失败"})
+
+
+# 结束一盘游戏
+@app.route("/end_game", methods=['POST'])
+async def end_game():
+    """
+        结束一盘游戏
+    """
+    return make_json_response({"message": "五子棋游戏结束成功"})
+    # return make_json_response({"message": "游戏结束失败"})
+
+
+# 下一步
+@app.route("/step", methods=['POST'])
+async def next_step():
+    """
+        下一步
+    """
+    arr = [[0 for i in range(6)] for j in range(6)]
+    arr[0][0] = 1
+    arr[1][3] = 2
+    x = request.json.get('x', 0)
+    y = request.json.get('y', 0)
+    # 如果处理成功，返回“处理成功”消息，并且返回当前棋盘，否则返回错误消息
+    flag = True
+    if flag:
+        return make_json_response({"message": "AI下一步成功。下面是现在的棋盘，其中：1代表黑子，2代表白子，0代表空，"
+                                              "文心一言需要根据这个数组生成markdown版表格6x6棋盘，黑子用O表示，白子用X表示。",
+                                   "chessboard": arr})
+    else:
+        return make_json_response({"message": "下一步失败"})
+
+
 @app.route("/read_board", methods=['POST'])
 async def read_board():
+    # 输出所有参数
+    print(request.json)
     """
-        读取远程文件中的棋盘
+        读取棋盘
     """
     url = request.json.get('url', "")
     import requests
@@ -119,11 +170,10 @@ async def openapi_spec():
         return text, 200, {"Content-Type": "text/yaml"}
 
 
-
-
 @app.route('/')
 def index():
     return 'welcome to my webpage!'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=8081)
